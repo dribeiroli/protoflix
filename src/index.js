f31e5d49d8fddedef8ed27b9821fb6a4ff99bf5e
dribeiroli <adrisenna@gmail.com>
2020-07-28 21:20:36 -0300

    Segunda parte do projeto

--- src/index.js
+++ src/index.js
@@ -1,13 +1,36 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import Home from './pages/Home/';
+import CadastroVideo from './pages/cadastro/Video'
+import CadastroCategoria from './pages/cadastro/Categoria'
+
+import {
+  BrowserRouter,
+  Switch,
+  Route
+} from "react-router-dom";
+
+
+
+const Pagina404 = () => (<div>Página não encontrada - ERRO 404</div>)
 
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <BrowserRouter>
+    <Switch>
+
+      <Route path="/cadastro/video" component={CadastroVideo} />
+      <Route path="/cadastro/categoria" component={CadastroCategoria} />
+      <Route path="/" component={Home} exact />
+      <Route component={Pagina404} />
+
+    </Switch>
+
+
+
+
+  </BrowserRouter>,
   document.getElementById('root')
 );
 
